bf16d3c0d1b84a6740a83ebc4d9c778756d8b6bb
Ryotarou Shiozawa <pekoemood@gmail.com>
2026-03-03 23:51:15 +0900

    inertia学習6日目

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use Dom\Comment;
 use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -10,3 +11,6 @@ use Inertia\Inertia;
 Route::get('/', [PostController::class, 'index'])->name('post.index');
 Route::get('/register', [Authenticatable::class, 'register'])->name('auth.register');
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('post.show');
+Route::post('/post/{post}', [PostController::class, 'store'])->name('post.store');
+Route::post('/post/{post}/comment',[Comment::class, 'store'])->name('comment.store');
+
