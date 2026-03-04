853e029d32ed7a97555ac9d4f553b3f087c8d5ae
Ryotarou Shiozawa <pekoemood@gmail.com>
2026-03-04 23:28:04 +0900

    inertia学習7日目

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -19,5 +20,10 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        $users = User::factory()->count(10)->create();
+        foreach( $users as $user) {
+            $user->posts()->createMany(Post::factory(10)->make()->toArray());
+        }
     }
 }
